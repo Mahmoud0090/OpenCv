d6c4ee81d34ab706c7e9a22ed7e6f06ecc88cd3f
Mahmoud0090 <mmh.00090@gmail.com>
2023-02-26 20:26:13 +0100

    read images and videos

---
+++ opencv/read.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+
+
+# reading image
+# img = cv.imread("photos\Pic.png")
+#
+# cv.imshow("n" , img)
+#
+# cv.waitKey(0)
+
+#reading video
+
+capture = cv.VideoCapture("videos/game.mp4")
+
+while True:
+    Istrue , frame = capture.read()
+    cv.imshow("video" , frame)
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
+
+
+
