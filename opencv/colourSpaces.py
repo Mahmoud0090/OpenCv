82b9434f66d6e0aceaa31cc2260fd73649ea0dbe
Mahmoud0090 <mmh.00090@gmail.com>
2023-03-05 22:36:39 +0100

    colourSpaces

---
+++ opencv/colourSpaces.py
@@ -0,0 +1,38 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+img = cv.imread('photos/cat.jpg')
+
+img = cv.resize(img , (500,500))
+
+cv.imshow('img' , img)  # will change automatically BGR to RGB
+
+rgbImg = cv.cvtColor(img , cv.COLOR_BGR2RGB)
+
+# the default color of open cv is BGR and matplotlib is RGB
+# pltimg = plt.imshow(rgbImg)
+# plt.show()
+
+#BGR to grayscal
+
+gray = cv.cvtColor(img , cv.COLOR_BGR2GRAY)
+cv.imshow('gray' , gray)
+
+# BGR to HSV
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+cv.imshow('HSV', hsv)
+
+# BGR to L*a*b
+lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+cv.imshow('LAB', lab)
+
+
+# BGR to RGB
+rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+cv.imshow('RGB', rgb)
+
+# HSV to BGR
+lab_bgr = cv.cvtColor(lab, cv.COLOR_LAB2BGR)
+cv.imshow('LAB --> BGR', lab_bgr)
+
+cv.waitKey(0)
